976e5dc1a268090bad4f81752b7f4e2868937853
Ian Harris <ianwebsterharris@gmail.com>
2019-12-02 22:22:59 -0700

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # prettier-config
 personal prettier config
+
+## Installation and usage
+
+```bash
+npm install --save-dev @iwharris/prettier-config
+```
+
+In `prettier.config.js`:
+
+```javascript
+module.exports = require('@iwharris/prettier-config');
+```
---
+++ index.ts
@@ -0,0 +1,15 @@
+import { Options } from 'prettier';
+
+const config: Options = {
+    "arrowParens": "always",
+    "bracketSpacing": true,
+    "endOfLine": "lf",
+    "printWidth": 100,
+    "semi": true,
+    "singleQuote": true,
+    "tabWidth": 4,
+    "trailingComma": "es5",
+    "useTabs": false
+};
+
+export default config;
